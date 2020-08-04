f008d9259dfc95f135d7a28dbe931cb6c16bea0f
paulzay <paulotieno2@gmail.com>
2020-08-04 18:07:59 +0300

    add signup component and style the parent component

--- src/pages/signup-signin/signup-signin.js
+++ src/pages/signup-signin/signup-signin.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./signup-signin.scss";
-import SignIn from '../../components/sign-in/SignIn';
-
-const SignupSigninPage = () => <div className="signup-signin">
-  <SignIn/>
-</div>;
+import SignIn from "../../components/sign-in/SignIn";
+import SignUp from "../../components/sign-up/signup";
+const SignupSigninPage = () => (
+  <div className="signup-signin">
+    <SignIn />
+    <SignUp />
+  </div>
+);
 
 export default SignupSigninPage;
