2676d58c4f577a43f220dc4edbfd015ec0c06f4b
paulzay <paulotieno2@gmail.com>
2020-08-04 18:56:31 +0300

    add redux and redux-logger and remove css

--- src/components/sign-up/signup.js
+++ src/components/sign-up/signup.js
@@ -19,7 +19,7 @@ class Signup extends React.Component {
     event.preventDefault();
     const { displayName, email, password, confirmPassword } = this.state;
 
-    if (password == confirmPassword) {
+    if (password !== confirmPassword) {
       alert("Passwords don't match");
       return;
     }
@@ -79,7 +79,7 @@ class Signup extends React.Component {
           <FormInput
             type="password"
             name="password"
-            value={password}
+            value={confirmPassword}
             onChange={this.handleChange}
             label="Confirm Password"
             required
