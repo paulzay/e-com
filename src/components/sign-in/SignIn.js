0934045c3b74343280478845ddd960e950c66d13
paulzay <paulotieno2@gmail.com>
2020-08-04 18:09:59 +0300

    finish with signin component

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -2,7 +2,7 @@ import React from "react";
 import FormInput from "../form-input/FormInput";
 import CustomButton from "../custom-btn/CustomButton";
 import "./sign-in.style.scss";
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -14,10 +14,17 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = (event) => {
@@ -49,7 +56,12 @@ class SignIn extends React.Component {
             label="password"
             required
           />
-          <CustomButton onClick={signInWithGoogle}> Sign in with Google</CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit"> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
