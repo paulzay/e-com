ec0e47cad995028aef5c04c4e05759035eb3c484
paulzay <paulotieno2@gmail.com>
2020-08-16 00:12:47 +0300

    implement dropdown functionality

--- src/components/cart-icon/CartIcon.js
+++ src/components/cart-icon/CartIcon.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { connect } from 'react-redux';
+
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { ReactComponent as ShoppingIcon } from "../../assets/bag.svg";
 import "./cart-icon.scss";
 
-const CartIcon = () => (
-  <div className="cart-icon">
-    <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+const CartIcon = ({ toggleCartHidden }) => (
+  <div className='cart-icon' onClick={toggleCartHidden}>
+    <ShoppingIcon className='shopping-icon' />
+    <span className='item-count'>0</span>
   </div>
 );
 
-export default CartIcon;
+const mapDispatchToProps = dispatch => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden())
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CartIcon);
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,7 +8,7 @@ import CartDropdown from '../cart/cart-dropdown';
 import CartIcon from '../cart-icon/CartIcon';
 import { auth } from "../../firebase/firebase.utils";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -31,12 +31,13 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon/>
     </div>
-    <CartDropdown/>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
